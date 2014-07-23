d6a7be371304974745ca485ffe3a1c6d2582cef9
Will Chiong <wchiong@gilt.com>
2014-07-22 22:08:30 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+angular-chrome-untappd
+==================
+
+A client for untappd's api using angular for use in a chrome extension or
+application. You must have a registered api key with untappd.com.
+
+This will provide the ability to authenticate using untappd's oauth api, and the
+ability to retrieve
+
+Getting started with Untappd API
+--------------------------------
+
+1. You need to get yourself an Untappd ClientID and ClientSecret. To do so, go
+complete the [Untappd API Key Form](http://untappd.com/api/register?register=new)
+2. Create a chrome extension
+3. Find your chrome extensions - for more details view the [google documentation](https://developer.chrome.com/extensions/tut_oauth).
+Instructions
+------------
+
+### [Optional] Install the module using bower
+
+Install using bower install
+
+    bower install infomofo/angular-chrome-untappd
+
+Add the following script import
+
+```html
+    <script src="bower_components/angular-chrome-untappd/angular-chrome-untappd.js"></script>
+```
+
+### Import the untappd client module
+
+```javascript
+angular.module('myapp',['UntappdClient']);
+```
+
+### Add your config with your app id and password to your angular constants
+
+```javascript
+app.constant("UNTAPPD_CONFIG",{
+  CLIENT_ID: "INSERT YOUR UNTAPPD CLIENT ID HERE"
+});
+```
+
+### Make API Calls
+
+The following api calls are currently supported by the untappd client.
+
+* *UntappdClient.authenticate* - Returns a Promise of the authentication token for the specified client id with the given extension id
---
+++ angular-chrome-untappd.js
@@ -0,0 +1,139 @@
+'use strict';
+
+angular.module("UntappdClient",[])
+	.factory('UntappdClient', function($q, $http, UNTAPPD_CONFIG){
+		var _this = this;
+
+    var config = angular.extend({
+				BASE_URL : 'https://untappd.com',
+        API_BASE_URL : 'https://api.untappd.com/v4'
+    }, UNTAPPD_CONFIG);
+
+		this.extensionId = chrome.runtime.id;
+		this.authenticationUrl =
+			config.BASE_URL + '/oauth/authenticate?client_id=' + config.CLIENT_ID +
+			'&redirect_url=https://' + this.extensionId + '.chromiumapp.org/login&response_type=token';
+
+		var userInfoUrl = config.API_BASE_URL + "/user/info" + "?access_token=";
+		var friendsUrl = config.API_BASE_URL + "/user/friends/";
+
+		_this.token = null;
+
+		this.friendsUrl = function(userName) {
+			return friendsUrl + userName + "?access_token=" + _this.token;
+		}
+
+		this.getAuthenticationToken = function() {
+			_this.token = "authenticating";
+			var deferred = $q.defer();
+
+			// console.log(_this.token);
+			// console.log(_this.authenticationUrl);
+			chrome.identity.launchWebAuthFlow(
+				{'url': _this.authenticationUrl, 'interactive': true},
+					function(redirect_url) {
+						if (redirect_url == undefined) {
+							if (chrome.runtime.lastError) {
+									console.error(chrome.runtime.lastError);
+							} else {
+								console.log("there was an error in authentication");
+							}
+							_this.token = null;
+							deferred.resolve(null);
+						} else {
+						console.log(redirect_url);
+						var paramPartOfURL = redirect_url.slice(redirect_url.indexOf('#') + 1);
+						var returnVal = paramPartOfURL.slice(paramPartOfURL.indexOf('=') + 1);
+						console.log("returning authentication token " + returnVal);
+						_this.token = returnVal;
+						deferred.resolve(returnVal);
+					}
+				});
+			return deferred.promise;
+		}
+
+		this.getLoggedInUserData = function() {
+			console.log("retrieving user data with token " + _this.token);
+			if (typeof _this.token === 'undefined') {
+				return _this.getLoggedInUserObject();
+			}
+		}
+
+		this.getLoggedInUserObject = function() {
+			var deferred = $q.defer();
+			console.log(userInfoUrl + _this.token);
+
+			$http.post(userInfoUrl + _this.token).success(function(data) {
+				// console.log(data.response);
+				var user = data.response.user;
+					deferred.resolve(user);
+				});
+				return deferred.promise;
+		}
+
+		this.getFriendsObject = function(userName) {
+			var deferred = $q.defer();
+			console.log(_this.friendsUrl(userName));
+
+			$http.post(_this.friendsUrl()).success(function(data) {
+				// console.log(data.response);
+				var friends = data.response.items;
+				deferred.resolve(friends);
+				});
+				return deferred.promise;
+		}
+		return {
+			/**
+			 * @ngdoc method
+			 * @name UntappdClient.authenticate
+			 * This method retrieves an authentication token. It also stores the token
+			 * in local memory.
+			 *
+			 * @return {Promise} The value this promise will be resolved to will be
+			 * the authentication token
+			 */
+			authenticate: function() {
+				return _this.getAuthenticationToken();
+			},
+
+			/**
+			* @ngdoc method
+			* @name UntappdClient.logOut
+			* Removes the token from local memory.
+			*/
+			logOut: function() {
+				_this.token = null;
+			},
+
+			/**
+			*	@ngdoc method
+			* @name UntappdClient.isLoggedOut
+			*
+			* @return {Boolean} true if the user is logged in, false if they are logged out
+			*/
+			isLoggedIn: function() {
+				return angular.isDefined(_this.token) && _this.token != null;
+			},
+
+			/**
+			*	@ngdoc method
+			* @name UntappdClient.isAuthenticating
+			*
+			* @return {Boolean} true if the user is currently authenticating
+			*/
+			isAuthenticating: function() {
+				return _this.token == "authenticating";
+			},
+
+			/**
+			* @ngdoc method
+			* @name UntappdClient.getLoggedInUser
+			*
+			* @return {Promise} The value this promise will be resolved to will be the
+			* logged in user
+			*/
+			getLoggedInUser: function() {
+				return _this.getLoggedInUserObject();
+			}
+		}
+	});
